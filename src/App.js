20b67412d5e6f73683cfa17e34293a53315f75f8
amine <bouramtaneamine1@gmail.com>
2023-06-08 19:46:05 +0100

    condition logIn

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-import Welcome from './components/components_state_props_handling_events/Welcome';
-import WelcomeCC from './components/components_state_props_handling_events/WelcomeCC';
-import Event from './components/components_state_props_handling_events/Event';
-import Event2 from './components/components_state_props_handling_events/Event2';
+// import Welcome from './components/components_state_props_handling_events/Welcome';
+// import WelcomeCC from './components/components_state_props_handling_events/WelcomeCC';
+// import Event from './components/components_state_props_handling_events/Event';
+// import Event2 from './components/components_state_props_handling_events/Event2';
+import Conddition from './components/conditional_rendering_lists_and_keys/Conddition';
 
 function App() {
   return (
     <div className="App">
-
+      {/* 
       <Welcome name='Amine BOURAMTANE' age='21' dev='Full Stack Web Developper'/>
         <hr/>
       <WelcomeCC name='Amine BOURAMTANE'/>
       <hr/>
       <Event/>
       <hr/>
-      <Event2/>
+      <Event2/> */}
+
+      <Conddition />
 
     </div>
   );
